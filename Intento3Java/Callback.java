a279b8c29f1fc3ce15e58c6c68ffed4fea21900d
Lucía <55547174+LuciaMoralesRosa@users.noreply.github.com>
2024-04-30 08:43:08 +0200

    Actualizar Callback.java

--- Intento3Java/Callback.java
+++ Intento3Java/Callback.java
@@ -1,5 +1,13 @@
 import java.io.Serializable;
 
+/*******************************************************************************
+ * Autores: Lizer Bernad Ferrando, 779035
+ * Lucia Morales Rosa, 816906
+ * 
+ * Fichero: Callback.java
+ * Comentarios: Es el fichero correspondiente a la función callback pasada como parámetro por el consumidor.
+ ******************************************************************************/
+
 /**
  * Clase que representa un callback para ejecutar un mensaje.
  * Implementa la interfaz Serializable para permitir la serialización.
