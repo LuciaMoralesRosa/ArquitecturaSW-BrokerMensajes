57e524ce103141d6c2e42ae4482a4a8ef14f80f8
Lucía <55547174+LuciaMoralesRosa@users.noreply.github.com>
2024-04-30 09:09:33 +0200

    Actualizar ObjConsumidor.java

--- Intento3Java/ObjConsumidor.java
+++ Intento3Java/ObjConsumidor.java
@@ -4,35 +4,58 @@
  * 
  * Fichero: ObjComsumidor.java
  * Comentarios: Implementa el objeto ObjComsumidor. ******************************************************************************/
+
+/**
+ * Clase que representa un consumidor en el sistema MOM (Message Oriented Middleware).
+ */
 public class ObjConsumidor {
-	String nombreConsumidor;
-	Callback metodoCallback;
+    String nombreConsumidor;
+    Callback metodoCallback;
 
-	
-	public ObjConsumidor(String nombreConsumidor, Callback metodoCallback) {
-		this.nombreConsumidor = nombreConsumidor;
-		this.metodoCallback = metodoCallback;
-	}
+    /**
+     * Constructor de la clase ObjConsumidor.
+     *
+     * @param nombreConsumidor Nombre del consumidor.
+     * @param metodoCallback   Callback asociado al consumidor.
+     */
+    public ObjConsumidor(String nombreConsumidor, Callback metodoCallback) {
+        this.nombreConsumidor = nombreConsumidor;
+        this.metodoCallback = metodoCallback;
+    }
 
+    /**
+     * Método para obtener el nombre del consumidor.
+     *
+     * @return El nombre del consumidor.
+     */
+    public String getNombreConsumidor() {
+        return nombreConsumidor;
+    }
 
-	public String getNombreConsumidor() {
-		return nombreConsumidor;
-	}
+    /**
+     * Método para establecer el nombre del consumidor.
+     *
+     * @param nombreConsumidor El nuevo nombre del consumidor.
+     */
+    public void setNombreConsumidor(String nombreConsumidor) {
+        this.nombreConsumidor = nombreConsumidor;
+    }
 
+    /**
+     * Método para obtener el callback asociado al consumidor.
+     *
+     * @return El callback asociado al consumidor.
+     */
+    public Callback getMetodoCallback() {
+        return metodoCallback;
+    }
 
-	public void setNombreConsumidor(String nombreConsumidor) {
-		this.nombreConsumidor = nombreConsumidor;
-	}
-
-
-	public Callback getMetodoCallback() {
-		return metodoCallback;
-	}
-
-
-	public void setMetodoCallback(Callback metodoCallback) {
-		this.metodoCallback = metodoCallback;
-	}
-
-	
+    /**
+     * Método para establecer el callback asociado al consumidor.
+     *
+     * @param metodoCallback El nuevo callback asociado al consumidor.
+     */
+    public void setMetodoCallback(Callback metodoCallback) {
+        this.metodoCallback = metodoCallback;
+    }
 }
