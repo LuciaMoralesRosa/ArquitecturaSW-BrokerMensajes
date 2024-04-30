0f674758d520f4f1eca4f4d8856160aee37f1003
Lucía <55547174+LuciaMoralesRosa@users.noreply.github.com>
2024-04-30 08:42:10 +0200

    Actualizar Callback.java

--- Intento3Java/Callback.java
+++ Intento3Java/Callback.java
@@ -1,7 +1,18 @@
 import java.io.Serializable;
 
+/**
+ * Clase que representa un callback para ejecutar un mensaje.
+ * Implementa la interfaz Serializable para permitir la serialización.
+ */
 public class Callback implements Serializable {
-	public void ejecutarMsj(String mensaje){
-		System.out.println("El mensaje recibido es: " + mensaje);
-	}
+
+    /**
+     * Método para ejecutar un mensaje.
+     * Imprime el mensaje recibido por consola.
+     *
+     * @param mensaje El mensaje a ser ejecutado.
+     */
+    public void ejecutarMsj(String mensaje){
+        System.out.println("El mensaje recibido es: " + mensaje);
+    }
 }
